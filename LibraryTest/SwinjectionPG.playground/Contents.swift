7688b8335a81a424a8173054e47c3a5893c85a46
iia.m.y <frezhgrand@gmail.com>
2022-06-25 10:10:18 +0300

    delete untracked files

--- LibraryTest/SwinjectionPG.playground/Contents.swift
+++ LibraryTest/SwinjectionPG.playground/Contents.swift
@@ -1,3 +1,41 @@
-import UIKit
+import Swinjection
 
-var greeting = "Hello, playground"
+let container = ServiceLocator.shared
+
+class A {
+    let x: Int
+    let b: B
+    
+    init(x: Int, b: B) {
+        self.x = x
+        self.b = b
+    }
+}
+
+class B {
+    let y: Int
+    
+    init(y: Int) {
+        self.y = y
+    }
+}
+
+class C {
+    let b: B
+    
+    init(b: B) {
+        self.b = b
+    }
+}
+
+container.register(service: B.self, scope: ObjectScope.singleton) { _ in
+    return B(y: 1)
+}
+container.register(service: A.self, scope: ObjectScope.singleton) { r in
+    return A(x: 0, b: r.resolve()!)
+}
+
+
+guard let a: A = container.resolve() else { fatalError() }
+print(a.b.y)
+
--- Swinjection/ObjectScope.swift
+++
@@ -1,9 +0,0 @@
-//
-// Created by IlyaCool on 21.06.22.
-//
-
-import Foundation
-
-public enum ObjectScope {
-    case transient, singleton
-}
